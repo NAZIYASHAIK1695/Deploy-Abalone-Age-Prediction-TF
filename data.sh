d138c2ce608994d2acb713a5c31b5cf77ffc3706
Amar Mattaparthi <89722343+amar-m-cloud@users.noreply.github.com>
2022-08-27 00:24:20 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo yum update
+sudo yum update -y
 sudo yum install git -y
 sudo amazon-linux-extras install nginx1 -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo systemctl status nginx
 sudo rm -r /etc/nginx/nginx.conf
-git clone https://github.com/amar-m-cloud/Abalone-Age-Prediction.git
-cd Abalone-Age-Prediction
+git clone https://github.com/amar-m-cloud/Abalone-Age-Prediction.git /home/ec2-user/
+cd /home/ec2-user/Abalone-Age-Prediction
 sudo cp -r nginx.conf /etc/nginx/
 sudo systemctl restart nginx
 pip3 install -r requirements.txt
